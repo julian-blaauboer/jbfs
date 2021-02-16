0d498c29d3ccb9b76c511732ba8518378b0550aa
Julian Blaauboer <julian.blaauboer@gmail.com>
2021-02-16 01:14:11 +0100

    Reimplemented file truncation
    File truncation has been reimplemented. Errors during deallocation are
    silently ignored. I might add a printk notifying the user something went
    wrong, but there is not much that they can do. Perhaps failed
    deallocations could get logged and later replayed.

--- balloc.c
+++ balloc.c
@@ -183,6 +183,7 @@ int jbfs_new_blocks_cont(struct inode *inode, sector_t iblock, u64 *bno,
 			 int max, struct buffer_head *bh,
 			 struct jbfs_raw_extent *raw)
 {
+	struct super_block *sb = inode->i_sb;
 	struct cont_node *node = (struct cont_node *)bh->b_data;
 	struct buffer_head *next_bh;
 	int size, err = 0;
@@ -194,7 +195,7 @@ int jbfs_new_blocks_cont(struct inode *inode, sector_t iblock, u64 *bno,
 
 	for(;;) {
 		struct jbfs_raw_extent *end =
-		    (struct jbfs_raw_extent *)(bh->b_data + bh->b_size);
+		    (struct jbfs_raw_extent *)(bh->b_data + sb->s_blocksize);
 
 		mark_buffer_dirty(bh);
 
@@ -276,6 +277,7 @@ int jbfs_new_blocks_local(struct inode *inode, sector_t iblock, u64 *bno,
 int jbfs_get_blocks(struct inode *inode, sector_t iblock, u64 *bno, int max,
 		    int create, bool *new, bool *boundary)
 {
+	struct super_block *sb = inode->i_sb;
 	struct jbfs_inode_info *ji = JBFS_I(inode);
 	struct buffer_head *bh;
 	u64 cont = ji->i_cont;
@@ -315,7 +317,7 @@ int jbfs_get_blocks(struct inode *inode, sector_t iblock, u64 *bno, int max,
 		struct jbfs_raw_extent *raw, *end;
 		struct jbfs_extent extent;
 
-		bh = sb_bread(inode->i_sb, cont);
+		bh = sb_bread(sb, cont);
 		if (!bh)
 			return -EIO;
 
@@ -323,7 +325,7 @@ int jbfs_get_blocks(struct inode *inode, sector_t iblock, u64 *bno, int max,
 		cont = le64_to_cpu(node->next);
 		size = le64_to_cpu(node->length);
 
-		end = (struct jbfs_raw_extent *)(bh->b_data + bh->b_size);
+		end = (struct jbfs_raw_extent *)(bh->b_data + sb->s_blocksize);
 
 		if (size >= iblock && cont) {
 			iblock -= size;
@@ -392,4 +394,160 @@ int jbfs_get_block(struct inode *inode, sector_t iblock,
 	return 0;
 }
 
-void jbfs_truncate(struct inode *inode) {}
+static void jbfs_dealloc_blocks(struct super_block *sb, u64 start, u64 size)
+{
+	struct buffer_head *bh;
+	struct jbfs_sb_info *sbi = JBFS_SB(sb);
+	u64 group = jbfs_block_extract_group(sbi, start);
+	u64 local = jbfs_block_extract_local(sbi, start);
+	u64 limit = sbi->s_group_data_blocks;
+	u64 block = jbfs_group_refmap_start(sbi, group) +
+		    (local >> sb->s_blocksize_bits);
+	int i = local & (sb->s_blocksize - 1);
+
+	if (start + size > sbi->s_num_blocks)
+		return;
+
+	JBFS_GROUP_LOCK(sbi, group);
+
+	bh = sb_bread(sb, block);
+	if (!bh)
+		goto out;
+
+	mark_buffer_dirty(bh);
+
+	for (; local < limit && size; ++local, ++i, --size) {
+		if (i == sb->s_blocksize) {
+			i = 0;
+			brelse(bh);
+			bh = sb_bread(sb, ++block);
+			if (!bh)
+				goto out;
+
+			mark_buffer_dirty(bh);
+		}
+
+		((u8 *)bh->b_data)[i] -= 1;
+	}
+
+	brelse(bh);
+
+out:
+	JBFS_GROUP_UNLOCK(sbi, group);
+}
+
+void jbfs_truncate(struct inode *inode)
+{
+	struct super_block *sb = inode->i_sb;
+	struct jbfs_inode_info *ji = JBFS_I(inode);
+	u64 cont = ji->i_cont;
+	u64 blocks;
+	u64 size;
+	int i;
+
+	blocks = (inode->i_size + sb->s_blocksize - 1) >> sb->s_blocksize_bits;
+
+	block_truncate_page(inode->i_mapping, inode->i_size, jbfs_get_block);
+
+	for (i = 0; i < JBFS_INODE_EXTENTS && blocks; ++i) {
+		if (jbfs_extent_empty(&ji->i_extents[i]))
+			goto done;
+
+		size = jbfs_extent_size(&ji->i_extents[i]);
+		if (blocks >= size) {
+			blocks -= size;
+			continue;
+		}
+
+		ji->i_extents[i].end = ji->i_extents[i].start + blocks;
+		jbfs_dealloc_blocks(sb, ji->i_extents[i].end, size - blocks);
+	}
+
+	for (; i < JBFS_INODE_EXTENTS; ++i) {
+		if (jbfs_extent_empty(&ji->i_extents[i]))
+			goto done;
+
+		size = jbfs_extent_size(&ji->i_extents[i]);
+		jbfs_dealloc_blocks(sb, ji->i_extents[i].start, size);
+		ji->i_extents[i].start = ji->i_extents[i].end = 0;
+	}
+
+	if (!blocks)
+		ji->i_cont = 0;
+
+	while (cont) {
+		struct buffer_head *bh;
+		struct cont_node *node;
+		struct jbfs_raw_extent *raw, *end;
+		struct jbfs_extent extent;
+		bool empty = !blocks;
+		u64 next;
+		u64 length;
+
+		bh = sb_bread(sb, cont);
+		if (!bh)
+			goto done;
+
+		mark_buffer_dirty(bh);
+
+		node = (struct cont_node *)bh->b_data;
+		end = (struct jbfs_raw_extent *)(bh->b_data + sb->s_blocksize);
+
+		next = le64_to_cpu(node->next);
+		length = le64_to_cpu(node->length);
+		if (blocks >= length) {
+			blocks -= length;
+			goto done_cont;
+		}
+
+		for (raw = node->extents; raw != end && blocks; ++raw) {
+			extent.start = le64_to_cpu(raw->start);
+			extent.end = le64_to_cpu(raw->end);
+
+			if (jbfs_extent_empty(&extent))
+				goto done_cont;
+
+			size = jbfs_extent_size(&extent);
+			if (blocks >= size) {
+				blocks -= size;
+				continue;
+			}
+
+			extent.end = extent.start + blocks;
+			jbfs_dealloc_blocks(sb, extent.end, size - blocks);
+
+			raw->end = cpu_to_le64(extent.end);
+		}
+
+		for (; raw != end; ++raw) {
+			extent.start = le64_to_cpu(raw->start);
+			extent.end = le64_to_cpu(raw->end);
+
+			if (jbfs_extent_empty(&extent))
+				goto done_cont;
+
+			size = jbfs_extent_size(&extent);
+			jbfs_dealloc_blocks(sb, extent.start, size);
+
+			raw->start = raw->end = 0;
+			length -= size;
+		}
+
+done_cont:
+		node->length = cpu_to_le64(length);
+
+		if (!blocks)
+			node->next = 0;
+
+		brelse(bh);
+
+		if (empty)
+			jbfs_dealloc_blocks(sb, cont, 1);
+
+		cont = next;
+	}
+
+done:
+	inode->i_mtime = inode->i_ctime = current_time(inode);
+	mark_inode_dirty(inode);
+}
